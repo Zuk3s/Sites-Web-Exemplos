4e490ca2cbbe57818101b5989b82f737a7d8e3bf
Zuk3s <felipemarcedo_@hotmail.com>
2023-10-29 17:15:16 -0300

    Cursor Transition

--- Site Parallax/script.js
+++ Site Parallax/script.js
@@ -16,22 +16,27 @@ document.addEventListener('DOMContentLoaded', function () {
         hill1.style.top = value * 1.0 + 'px';
     });
 });
+const cursor = document.getElementById("circle-cursor");
 
 document.addEventListener("mousemove", (e) => {
-    const cursor = document.getElementById("circle-cursor");
-
     cursor.style.left = e.clientX - cursor.offsetWidth / 2 + "px";
     cursor.style.top = e.clientY - cursor.offsetHeight / 2 + "px";
 
     const targetElement = document.elementFromPoint(e.clientX, e.clientY);
 
-
     if (targetElement) {
+        cursor.style.backgroundColor = "#ffffffd8";
+        cursor.style.display = 'block';
         if (targetElement.tagName === "H2") {
             cursor.style.backgroundColor = "#007c63";
-        }
-        else {
-            cursor.style.backgroundColor = "#ffffffd8";
+        } else if (targetElement.tagName === "HEADER" || targetElement.tagName === "NAV") {
+            cursor.style.backgroundColor = "#007c63";
+            cursor.style.transform = "scale(1)";
+        } else if (targetElement.tagName === "A") {
+            cursor.style.transform = "scale(0.35)";
+        } else if (document.onmouseout) {
+            cursor.style.display = "none";
         }
     }
 });
+
